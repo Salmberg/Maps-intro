69c2806a0bc502630557c95baeb67cfe32ae9e97
Salmberg <60116309+Salmberg@users.noreply.github.com>
2023-05-09 14:32:44 +0200

    Initial Commit

---
+++ Maps-intro/Maps_introApp.swift
@@ -0,0 +1,17 @@
+//
+//  Maps_introApp.swift
+//  Maps-intro
+//
+//  Created by David Salmberg on 2023-05-09.
+//
+
+import SwiftUI
+
+@main
+struct Maps_introApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
